d5d39a73e3a922de07d2e09e2f55f9045dff280d
Shivani <shivanikumari2001@gmail.com>
2025-08-15 12:23:13 +0530

    Added by me

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const age = 24;
-  const name = "piyush Garg";
+  const name1 = "Yash Garg";
   const dob ="01/01/2000";
 
-
+function sayName(){
+    console.log("My name is" + name1);
+}
+function isAbove(){
+    return age>= 18;
+}
 
 
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 function sayName(){
-    console.log("Shivani")
+    console.log("Shivani")  
+    
 }
