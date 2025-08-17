43cf1d9b52a24af1c2ca14513a7d1d884e9a8e71
Shivani <shivanikumari2001@gmail.com>
2025-08-17 16:34:28 +0530

    new feat added

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const age = 24;
   const name1 = "Yash Garg";
   const dob ="01/01/2000";
 
-function sayName(){
-    console.log("My name is" + name1);
-}
 function isAbove(){
     return age>= 18;
 }
 
 
+function newFeatureAdd(){
+  console.log("this is a new feature");
+
+}
